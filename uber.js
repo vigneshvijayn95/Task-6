fd14e9d2bf3b1bab42163a9ecebed7f8f5db6383
vigneshvijayan95 <vickyjasper95@gmail.com>
2023-12-10 15:07:44 +0530

    Task-6 Completed

---
+++ uber.js
@@ -0,0 +1,16 @@
+var baseFee = 100
+var cities = ["Annanagar", "Nungambakkam", "Marina", ]
+var uberRates = [200, 300, 400]
+
+var customerName = "Vicky" 
+var  customerCity ="Annanagar"
+
+function getRate(Annanagar) {
+    
+    function uberRate(Annanagar) {
+      
+      var finalRate = (uberRates[200]) * baseFee
+      return finalRate
+    }
+}
+console.log(finalRate)
